33150cf4df4c6c24e012607a314ea620c01b4c6c
D.Balzamo <D.Balzamo@almaviva.it>
2026-03-17 15:12:54 +0100

    Aggiornamento libreria common

--- src/main/java/com/almaviva/libreria_common/entity/EntityBase.java
+++ src/main/java/com/almaviva/libreria_common/entity/EntityBase.java
@@ -1,12 +1,14 @@
 package com.almaviva.libreria_common.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@Data
 public class EntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
